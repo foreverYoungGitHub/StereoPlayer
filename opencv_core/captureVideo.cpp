eb9c61d8d8d2fe85118d18bd8ad072c200ef400d
Young <yliu344@uottawa.ca>
2016-10-20 10:08:52 -0400

    try opencv core

---
+++ opencv_core/captureVideo.cpp
@@ -0,0 +1,222 @@
+#include "captureVideo.hpp"
+
+captureVideo::captureVideo(std::string urlL, Mode mode, bool stereo)
+{
+	vector<string> stream_source;
+	stream_source.push_back(urlL);
+
+	captureVideo::captureVideo(stream_source);
+}
+
+captureVideo::captureVideo(string urlL, string urlR)
+{
+	vector<string> stream_source;
+	stream_source.push_back(urlL);
+	stream_source.push_back(urlR);
+
+	captureVideo::captureVideo(stream_source);
+}
+
+captureVideo::captureVideo(string urlL, string urlR, int screenWidth, int screenHeight)
+{
+	vector<string> stream_source;
+	stream_source.push_back(urlL);
+	stream_source.push_back(urlR);
+	width_ = screenWidth;
+	height_ = screenHeight;
+
+	captureVideo::captureVideo(stream_source);
+}
+
+captureVideo::captureVideo(vector<string> stream_source, Mode mode, bool stereo)
+{
+	camera_source = stream_source;
+	camera_count = camera_source.size();
+	isUSBCamera = false;
+
+	startCapture();
+}
+
+captureVideo::captureVideo(vector<int> capture_index)
+{
+	camera_index = capture_index;
+	camera_count = capture_index.size();
+	isUSBCamera = true;
+
+	startCapture();
+}
+
+captureVideo::~captureVideo()
+{
+	stopCapture();
+}
+
+
+
+void captureVideo::captureStereoFrame()
+{
+	VideoCapture *capture = camera_capture[0];
+	Mat frame;
+	frames_.push_back(frame);
+	frames_.push_back(frame);
+	mode_ = UPDOWN;
+	while (true)
+	{		
+		//Grab frame from camera capture
+		(*capture) >> frame;
+		if (!frame.empty())
+		{
+			//check the stereo way and resize it to suitable size
+			switch (mode_)
+			{
+			case LEFTRIGHT:	//the url is a left-right stereo video
+			{
+				if (!(frame.rows == height_ && frame.cols == width_ * 2))
+					resize(frame, frame, Size(width_ * 2, height_));
+				//Put frame to the queue
+				if (realtime)
+				{
+					writeState_[0] = true;
+					frames_[0] = frame(Rect(0, 0, width_, height_));
+					writeState_[1] = false;
+					writeState_[1] = true;
+					frames_[1] = frame(Rect(width_, 0, width_, height_));
+					writeState_[1] = false;
+				}
+				//Put frame to the member variabale vector
+				else
+				{
+					frame_queue[0]->push(frame(Rect(0, 0, width_, height_)));
+					frame_queue[0]->push(frame(Rect(width_, 0, width_, height_)));
+				}
+
+			}
+			case UPDOWN: //the url is a up-down stereo video
+			{
+				if (!(frame.rows == height_ * 2 && frame.cols == width_))
+					resize(frame, frame, Size(width_, height_ * 2));
+				//Put frame to the queue
+				if (realtime)
+				{
+					writeState_[0] = true;
+					frames_[0] = frame(Rect(0, 0, width_, height_));
+					writeState_[1] = false;
+					writeState_[1] = true;
+					frames_[1] = frame(Rect(0, height_, width_, height_));
+					writeState_[1] = false;
+				}
+				//Put frame to the member variabale vector
+				else
+				{
+					frame_queue[0]->push(frame(Rect(0, 0, width_, height_)));
+					frame_queue[0]->push(frame(Rect(0, height_, width_, height_)));
+				}
+
+			}
+			default:
+				break;
+			}
+		}				
+	}
+	//relase frame resource
+	frame.release();
+}
+
+void captureVideo::captureFrame(int index)
+{
+	VideoCapture *capture = camera_capture[index];
+	Mat frame;
+	frames_.push_back(frame);
+	while (true)
+	{
+		//Grab frame from camera capture
+		(*capture) >> frame;
+		if(!frame.empty())
+		{ 
+			//resize it to suitable size
+			if (!(frame.rows == height_ && frame.cols == width_))
+				resize(frame, frame, Size(width_, height_));
+			//Put frame to the queue
+			if (realtime)
+			{
+				writeState_[index] = true;
+				frames_[index] = frame;
+				writeState_[index] = false;
+			}
+				
+			//Put frame to the member variabale vector
+			else
+				frame_queue[index]->push(frame);
+		}		
+	}
+	//relase frame resource
+	frame.release();
+}
+
+
+void captureVideo::startCapture()
+{
+	VideoCapture *capture;
+	thread *t;
+	concurrent_queue<Mat> *q;
+	bool writeState = true;
+
+	for (int i = 0; i < camera_count; i++)
+	{
+		//Make VideoCapture instance
+		if (!isUSBCamera) {
+			string url = camera_source[i];
+			capture = new VideoCapture(url);
+			cout << "Camera Setup: " << url << endl;
+		}
+		else {
+			int idx = camera_index[i];
+			capture = new VideoCapture(idx);
+			cout << "Camera Setup: " << to_string(idx) << endl;
+		}
+
+		//Put VideoCapture to the vector
+		camera_capture.push_back(capture);
+
+		//Put writing state
+		writeState_.push_back(writeState);
+
+		//Make thread instance
+		if(camera_count == 1)
+			t = new thread(&captureVideo::captureStereoFrame, this);
+		else
+			t = new thread(&captureVideo::captureFrame, this, i);
+
+		//Put thread to the vector
+		camera_thread.push_back(t);
+
+		if(!realtime)
+		{ 
+			//Make a queue instance
+			q = new concurrent_queue<Mat>;
+
+			//Put queue to the vector
+			frame_queue.push_back(q);
+		}
+	}
+}
+
+void captureVideo::stopCapture()
+{
+	VideoCapture *cap;
+	for (int i = 0; i < camera_count; i++)
+	{
+		cap = camera_capture[i];
+		if (cap->isOpened()) {
+			//Relase VideoCapture resource
+			cap->release();
+			cout << "Capture " << i << " released" << endl;
+		}
+	}
+}
+
+
+void captureVideo::setStereo(bool stereo)
+{
+	stereo_ = stereo;
+}
---
+++ opencv_core/main.cpp
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <tchar.h>
+#include <d3d9.h>
+#include <d3dx9.h>
+#include <Windows.h>
+#include <thread>
+
+#include "displayWindow.h"
+#include "captureVideo.hpp"
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+	std::string URL = "D:\\YangLiu\\Project\\bbb_sunflower_1080p_60fps_stereo_abl.mp4";
+
+	std::string URLLeft = "rtsp://192.168.0.100:8554/video1/unicast";
+	std::string URLRight = "rtsp://192.168.0.100:8554/video3/unicast";
+
+	vector<string> capture_source = {
+		"rtsp://192.168.0.100:8554/video1/unicast",
+		"rtsp://192.168.0.100:8554/video3/unicast"
+	};
+
+	//vector<string> capture_source = {
+	//	"D:\\YangLiu\\Project\\bbb_sunflower_1080p_60fps_stereo_abl.mp4"
+	//	//"rtsp://192.168.0.100:8554/unicast",
+	//	//"rtsp://192.168.0.100:8554/unicast"
+	//};
+
+	//vector<int> capture_source = {
+	//	1,
+	//	0
+	//};
+
+	displayWindow displayWindow_;
+	WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, (WNDPROC)displayWindow_.MyWndProc, 0L, 0L,
+		GetModuleHandle(NULL), NULL, NULL, NULL, NULL,
+		WINDOW_CLASS, NULL };
+
+	RegisterClassEx(&wc);
+
+	HWND hwnd = CreateWindow(WINDOW_CLASS, WINDOW_NAME, WS_OVERLAPPEDWINDOW,
+		0, 0, displayWindow_.width, displayWindow_.height, GetDesktopWindow(), NULL,
+		wc.hInstance, NULL);
+	if (hwnd == NULL) {
+		return -1;
+	}
+
+
+	captureVideo captureVideo_(capture_source);
+	
+
+	if (displayWindow_.InitD3D(hwnd, false))
+	{
+		ShowWindow(hwnd, SW_SHOWDEFAULT);
+		UpdateWindow(hwnd);
+
+		MSG msg;
+		ZeroMemory(&msg, sizeof(msg));
+
+		while (msg.message != WM_QUIT)
+		{
+			if (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+			else
+			{				
+				if (captureVideo_.realtime)
+				{
+					if(captureVideo_.writeState_[0] == false && captureVideo_.writeState_[1] == false) //make sure the mat is finishing writing
+					{
+						displayWindow_.Render(captureVideo_.frames_[0], captureVideo_.frames_[1]);
+					}					
+				}					
+				else
+				{
+					Mat leftImg, rightImg;
+					if (captureVideo_.camera_count == 1)
+					{
+						captureVideo_.frame_queue[0]->try_pop(leftImg);
+						captureVideo_.frame_queue[0]->try_pop(rightImg);
+					}
+					else
+					{
+						captureVideo_.frame_queue[0]->try_pop(leftImg);
+						captureVideo_.frame_queue[1]->try_pop(rightImg);
+					}
+
+					displayWindow_.Render(leftImg, rightImg);
+				}
+			}
+				
+		}
+	}
+
+	displayWindow_.Cleanup();
+	UnregisterClass("D3D", hInstance);
+	return 0;
+}
+
